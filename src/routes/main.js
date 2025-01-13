93cb0343e957ac0a19d43117e03504254d35c480
Edgar Corona <coronam.edgar@gmail.com>
2025-01-13 12:05:56 -0600

    Agenda por agendaId

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { dbPool } from "../db.js"
 import moment from "moment";
+import { generateUniqueCode } from "../utils.js";
 
 const mainRoutes = express.Router();
 
@@ -10,20 +11,29 @@ mainRoutes.get('/', (req, res) => {
 
 mainRoutes.get('/agendas', async (req, res) => {
     const agendasReq = await dbPool.query(`
-        SELECT *
+        SELECT fechaInicio,fechaFin,duracion
         FROM agendas
         WHERE fechaInicio >= NOW()
         AND fechaInicio < DATE_ADD(NOW(), INTERVAL 5 MONTH)`);
     res.send(agendasReq[0]);
 })
 
+mainRoutes.get('/agenda', async (req, res) => {
+    const { agendaId } = req.query;
+    const agendasReq = await dbPool.query(`
+        SELECT *
+        FROM agendas
+        WHERE agendaId=?`, [agendaId]);
+    res.send(agendasReq[0]);
+})
+
 mainRoutes.post('/agenda', async (req, res) => {
     const data = req.body;
-    const fechaInicio = moment(data.fecha).format("YYYY-MM-DD HH:00:00");
-    const finalDate = new Date(data.fecha);
-    finalDate.setHours(finalDate.getHours()+1+data.duracion);
+    const fechaInicio = moment(data.fechaInicio).format("YYYY-MM-DD HH:00:00");
+    const finalDate = new Date(data.fechaInicio);
+    finalDate.setHours(finalDate.getHours() + 1 + data.duracion);
     const fechaFin = moment(finalDate.toISOString()).format("YYYY-MM-DD HH:00:00");
-    
+
     const [rowAgendas] = await dbPool.query(`
             SELECT *
             FROM agendas
@@ -36,13 +46,17 @@ mainRoutes.post('/agenda', async (req, res) => {
         return;
     }
 
+    const agendaId = generateUniqueCode();
+
     const [reqData] = await dbPool.query(`
-        INSERT INTO agendas (nombre, telefono, email, fechaInicio, fechaFin, duracion) 
+        INSERT INTO agendas (nombre, telefono, email, fechaInicio, fechaFin, duracion, direccion, agendaId) 
         VALUES 
         (?, ?, ?, ?, ?, ?)`,
-        [data.nombre, data.telefono, data.email, fechaInicio, fechaFin, data.duracion]);
+        [data.nombre, data.telefono, data.email, fechaInicio, fechaFin, data.duracion, data.direccion, agendaId]);
     res.send({
-        id: reqData.insertId
+        id: reqData.insertId,
+        agendaId: agendaId
+
     });
 
 })
---
+++ src/utils.js
@@ -0,0 +1,18 @@
+export function generateUniqueCode() {
+    const now = new Date();
+    const year = now.getFullYear().toString().slice(-2);
+    const month = String(now.getMonth() + 1).padStart(2, '0');
+    const day = String(now.getDate()).padStart(2, '0');
+    const getRandomLetter = () => String.fromCharCode(65 + Math.floor(Math.random() * 26));
+    const getRandomNumber = () => Math.floor(Math.random() * 10);
+    let code = `${year}${month}${day}${getRandomLetter()}${getRandomLetter()}${getRandomLetter()}${getRandomNumber()}`;
+
+    code = code.split('');
+
+    for (let i = code.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [code[i], code[j]] = [code[j], code[i]]; // Intercambiar elementos
+    }
+
+    return code.join('');
+}
