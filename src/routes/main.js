c8052b29f242ec9d4df4cd853befe0b33ad3d665
Edgar Corona <coronam.edgar@gmail.com>
2025-01-13 12:13:05 -0600

    Errors

--- src/routes/main.js
+++ src/routes/main.js
@@ -19,7 +19,7 @@ mainRoutes.get('/agendas', async (req, res) => {
         res.send(agendasReq[0]);
     } catch (error) {
         res.status(400).send({
-            status: "E02",
+            status: "E00",
             message: error
         });
     }
@@ -27,45 +27,59 @@ mainRoutes.get('/agendas', async (req, res) => {
 })
 
 mainRoutes.get('/agenda', async (req, res) => {
-    const { agendaId } = req.query;
-    const agendasReq = await dbPool.query(`
+    try {
+        const { agendaId } = req.query;
+        const agendasReq = await dbPool.query(`
         SELECT *
         FROM agendas
         WHERE agendaId=?`, [agendaId]);
-    res.send(agendasReq[0]);
+        res.send(agendasReq[0]);
+    } catch (error) {
+        res.status(400).send({
+            status: "E00",
+            message: error
+        });
+    }
 })
 
 mainRoutes.post('/agenda', async (req, res) => {
-    const data = req.body;
-    const fechaInicio = moment(data.fechaInicio).format("YYYY-MM-DD HH:00:00");
-    const finalDate = new Date(data.fechaInicio);
-    finalDate.setHours(finalDate.getHours() + 1 + data.duracion);
-    const fechaFin = moment(finalDate.toISOString()).format("YYYY-MM-DD HH:00:00");
+    try {
+        const data = req.body;
+        const fechaInicio = moment(data.fechaInicio).format("YYYY-MM-DD HH:00:00");
+        const finalDate = new Date(data.fechaInicio);
+        finalDate.setHours(finalDate.getHours() + 1 + data.duracion);
+        const fechaFin = moment(finalDate.toISOString()).format("YYYY-MM-DD HH:00:00");
 
-    const [rowAgendas] = await dbPool.query(`
+        const [rowAgendas] = await dbPool.query(`
             SELECT id
             FROM agendas
             WHERE ? >= fechaInicio AND  ? < fechaFin`, [fechaInicio, fechaInicio]);
-    if (rowAgendas.length > 0) {
-        res.status(400).send({
-            status: "E01",
-            message: "Ya existe una agenda con esa fecha"
-        })
-        return;
-    }
+        if (rowAgendas.length > 0) {
+            res.status(400).send({
+                status: "E01",
+                message: "Ya existe una agenda con esa fecha"
+            })
+            return;
+        }
 
-    const agendaId = generateUniqueCode();
+        const agendaId = generateUniqueCode();
 
-    const [reqData] = await dbPool.query(`
+        const [reqData] = await dbPool.query(`
         INSERT INTO agendas (nombre, telefono, email, fechaInicio, fechaFin, duracion, direccion, agendaId) 
         VALUES 
         (?, ?, ?, ?, ?, ?)`,
-        [data.nombre, data.telefono, data.email, fechaInicio, fechaFin, data.duracion, data.direccion, agendaId]);
-    res.send({
-        id: reqData.insertId,
-        agendaId: agendaId
+            [data.nombre, data.telefono, data.email, fechaInicio, fechaFin, data.duracion, data.direccion, agendaId]);
+        res.send({
+            id: reqData.insertId,
+            agendaId: agendaId
 
-    });
+        });
+    } catch (error) {
+        res.status(400).send({
+            status: "E00",
+            message: error
+        });
+    }
 
 })
 
